f89eb2add36fec13aa24112b1cf867e2e20209a1
SonorousBanana <44536299+SonorousBanana@users.noreply.github.com>
2023-03-25 17:16:49 +0200

    Add files via upload

---
+++ ergasia1.js
@@ -0,0 +1,18 @@
+
+ 
+function myFun1() {
+ var x = document.getElementByid("desc");
+ 
+    x.style.display = "block";
+
+   
+  
+}
+
+function myFun2() {
+	
+ var y = document.getElementById("desc");
+  
+
+    y.style.display = "none";
+}
